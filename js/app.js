9f4f00198424a0bf3fb821ea41c11dcde0a5329a
vlad <ena-vlad@yandex.ru>
2021-12-01 22:47:37 +0400

    bob

---
+++ js/app.js
@@ -0,0 +1,43 @@
+
+const response = [
+    {
+        "index": 0,
+        "isActiv": true,
+        "age": 20,
+        "name": "ivan",
+        "gender": "man",
+        "email": "dhfdfjdhfdfjd",
+    },
+    {
+        "index": 1,
+        "isActiv": true,
+        "age": 30,
+        "name": "irland",
+        "gender": "woman",
+        "email": "dhfdfjdhfdfjd",
+    },
+    {
+        "index": 2,
+        "isActiv": true,
+        "age": 40,
+        "name": "ananas",
+        "gender": "man",
+        "email": "dhfdfjdhfdfjd",
+    },
+    {
+        "index": 3,
+        "isActiv": true,
+        "age": 50,
+        "name": "nada",
+        "gender": "woman",
+        "email": "dhfdfjdhfdfjd",
+    },
+
+
+
+
+];
+
+
+
+
